06785ede13dab4196327a664f497f7a329b84975
viexxxx <viqialvi11@gmail.com>
2023-12-11 11:24:25 +0700

    KODE SOAL FF

--- resources/views/footer2.blade.php
+++ resources/views/footer2.blade.php
@@ -1,6 +1,8 @@
 <footer>
     <hr style="width: 80%; height: 1px;">
+
     <p style="margin-left: 12%; text-align: left;">
+        <img src="copyright.png" style="width: 15px; height: 15px;">
         Viqi Alvianto. 2023
     </p>
 </footer>
